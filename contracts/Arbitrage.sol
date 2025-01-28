c95cebe54bb4b134d0ec04e62b742a5bd004c9f0
billhilly23 <138057807+billhilly23@users.noreply.github.com>
2025-01-28 10:35:12 -0500

    Update Arbitrage.sol

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -3,58 +3,58 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
 
 contract Arbitrage {
-    // Mapping of DEX addresses to token prices
-    mapping(address => uint256) public dexPrices;
+  // Mapping of DEX addresses to token prices
+  mapping(address => uint256) public dexPrices;
 
-    // Mapping of token balances
-    mapping(address => uint256) public tokenBalances;
+  // Mapping of token balances
+  mapping(address => uint256) public tokenBalances;
 
-    // Reentrancy guard
-    ReentrancyGuard public reentrancyGuard;
+  // Reentrancy guard
+  ReentrancyGuard public reentrancyGuard;
 
-    // Constructor
-    constructor(address[] memory _dexes, address _token) public {
-        // Initialize DEX addresses and token
-        for (uint256 i = 0; i < _dexes.length; i++) {
-            dexPrices[_dexes[i]] = 0;
-        }
-        token = _token;
+  // Constructor
+  constructor(address[] memory _dexes, address _token) public {
+    // Initialize DEX addresses and token
+    for (uint256 i = 0; i < _dexes.length; i++) {
+      dexPrices[_dexes[i]] = 0;
     }
+    token = _token;
+  }
 
-    // Arbitrage function
-    function arbitrage(address _dex1, address _dex2) public {
-        // Check for price discrepancies
-        uint256 price1 = dexPrices[_dex1];
-        uint256 price2 = dexPrices[_dex2];
+  // Arbitrage function
+  function arbitrage(address _dex1, address _dex2) public {
+    // Check for price discrepancies
+    uint256 price1 = dexPrices[_dex1];
+    uint256 price2 = dexPrices[_dex2];
 
-        if (price1 > price2) {
-            // Buy on DEX2 and sell on DEX1
-            buyOnDex(_dex2, price2);
-            sellOnDex(_dex1, price1);
-        } else if (price2 > price1) {
-            // Buy on DEX1 and sell on DEX2
-            buyOnDex(_dex1, price1);
-            sellOnDex(_dex2, price2);
-        }
+    if (price1 > price2) {
+      // Buy on DEX2 and sell on DEX1
+      buyOnDex(_dex2, price2);
+      sellOnDex(_dex1, price1);
+    } else if (price2 > price1) {
+      // Buy on DEX1 and sell on DEX2
+      buyOnDex(_dex1, price1);
+      sellOnDex(_dex2, price2);
     }
+  }
 
-    // Get price function
-    function getPrice(address _dex) public view returns (uint256) {
-        return dexPrices[_dex];
-    }
+  // Get price function
+  function getPrice(address _dex) public view returns (uint256) {
+    return dexPrices[_dex];
+  }
 
-    // Get balance function
-    function getBalance(address _token) public view returns (uint256) {
-        return tokenBalances[_token];
-    }
+  // Get balance function
+  function getBalance(address _token) public view returns (uint256) {
+    return tokenBalances[_token];
+  }
 
-    // Buy on DEX function
-    function buyOnDex(address _dex, uint256 _price) internal {
-        // Implement buy logic here
-    }
+  // Buy on DEX function
+  function buyOnDex(address _dex, uint256 _price) internal {
+    // Implement buy logic here
+  }
 
-    // Sell on DEX function
-    function sellOnDex(address _dex, uint256 _price) internal {
-        // Implement sell logic here
-    }
+  // Sell on DEX function
+  function sellOnDex(address _dex, uint256 _price) internal {
+    // Implement sell logic here
+  }
 }
