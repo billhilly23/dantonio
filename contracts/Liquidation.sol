1f24e6fa2f0906bc8a4a83b3041edf63358ae14b
billhilly23 <138057807+billhilly23@users.noreply.github.com>
2025-01-28 14:46:26 -0500

    Update Liquidation.sol

--- contracts/Liquidation.sol
+++ contracts/Liquidation.sol
@@ -4,119 +4,95 @@ import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/Re
 import "https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/UniswapV2Router02.sol";
 
 contract Liquidation {
-  // Mapping of DEX addresses to token prices
-  mapping(address => uint256) public dexPrices;
+    // Mapping of DEX addresses to token prices
+    mapping(address => uint256) public dexPrices;
 
-  // Mapping of token balances
-  mapping(address => uint256) public tokenBalances;
+    // Mapping of token balances
+    mapping(address => uint256) public tokenBalances;
 
-  // Reentrancy guard
-  ReentrancyGuard public reentrancyGuard;
+    // Reentrancy guard
+    ReentrancyGuard public reentrancyGuard;
 
-  // Constructor
-  constructor(address[] memory _dexes, address _token) public {
-    // Initialize DEX addresses and token
-    for (uint256 i = 0; i < _dexes.length; i++) {
-      dexPrices[_dexes[i]] = 0;
+    // Constructor
+    constructor(address[] memory _dexes, address _token) public {
+        // Initialize DEX addresses and token
+        for (uint256 i = 0; i < _dexes.length; i++) {
+            dexPrices[_dexes[i]] = 0;
+        }
+        token = _token;
     }
-    token = _token;
-  }
 
-  // Liquidation function
-  function liquidation(address _dex1, address _dex2) public {
-    // Check for price discrepancies
-    uint256 price1 = dexPrices[_dex1];
-    uint256 price2 = dexPrices[_dex2];
-
-    if (price1 > price2) {
-      // Buy on DEX2 and sell on DEX1
-      buyOnDex(_dex2, price2);
-      sellOnDex(_dex1, price1);
-    } else if (price2 > price1) {
-      // Buy on DEX1 and sell on DEX2
-      buyOnDex(_dex1, price1);
-      sellOnDex(_dex2, price2);
+    // Liquidation function
+    function liquidation(address _dex1, address _dex2) public {
+        // Check for price discrepancies
+        uint256 price1 = dexPrices[_dex1];
+        uint256 price2 = dexPrices[_dex2];
+        if (price1 > price2) {
+            // Buy on DEX2 and sell on DEX1
+            buyOnDex(_dex2, price2);
+            sellOnDex(_dex1, price1);
+        } else if (price2 > price1) {
+            // Buy on DEX1 and sell on DEX2
+            buyOnDex(_dex1, price1);
+            sellOnDex(_dex2, price2);
+        }
     }
-  }
 
-  // Get price function
-  function getPrice(address _dex) public view returns (uint256) {
-    return dexPrices[_dex];
-  }
+    // Buy on DEX function
+    function buyOnDex(address _dex, uint256 _price) internal {
+        // Set the Uniswap V2 Router address
+        address uniswapV2Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
-  // Get balance function
-  function getBalance(address _token) public view returns (uint256) {
-    return tokenBalances[_token];
-  }
+        // Set the WETH address
+        address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-  // Buy on DEX function
-  function buyOnDex(address _dex, uint256 _price) internal {
-    // Set the Uniswap V2 Router address
-    address uniswapV2Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+        // Set the token address
+        address token = address(this);
 
-    // Set the WETH address
-    address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        // Set the amount of ETH to swap
+        uint256 amountIn = 1 ether;
 
-    // Set the token address
-    address token = address(this);
+        // Set the amount of tokens to receive
+        uint256 amountOut = _price;
 
-    // Set the amount of ETH to swap
-    uint256 amountIn = 1 ether;
+        // Set the deadline for the transaction
+        uint256 deadline = block.timestamp + 15 minutes;
 
-    // Set the amount of tokens to receive
-    uint256 amountOut = _price;
+        // Create a path for the swap
+        address[] memory path = new address[](2);
+        path[0] = weth;
+        path[1] = token;
 
-    // Set the deadline for the transaction
-    uint256 deadline = block.timestamp + 15 minutes;
+        // Use the Uniswap V2 Router to swap ETH for the token
+        IUniswapV2Router02(uniswapV2Router).swapExactTokensForTokensSupportingFeeOnTransferTokens(amountIn, amountOut, path, _dex, deadline);
+    }
 
-    // Create a path for the swap
-    address[] memory path = new address[](2);
-    path[0] = weth;
-    path[1] = token;
+    // Sell on DEX function
+    function sellOnDex(address _dex, uint256 _price) internal {
+        // Set the Uniswap V2 Router address
+        address uniswapV2Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
-    // Use the Uniswap V2 Router to swap ETH for the token
-    IUniswapV2Router02(uniswapV2Router).swapExactTokensForTokensSupportingFeeOnTransferTokens(
-      amountIn,
-      amountOut,
-      path,
-      _dex,
-      deadline
-    );
-  }
+        // Set the WETH address
+        address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-  // Sell on DEX function
-  function sellOnDex(address _dex, uint256 _price) internal {
-    // Set the Uniswap V2 Router address
-    address uniswapV2Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+        // Set the token address
+        address token = address(this);
 
-    // Set the WETH address
-    address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        // Set the amount of tokens to swap
+        uint256 amountIn = _price;
 
-    // Set the token address
-    address token = address(this);
+        // Set the amount of ETH to receive
+        uint256 amountOut = 1 ether;
 
-    // Set the amount of tokens to swap
-    uint256 amountIn = _price;
+        // Set the deadline for the transaction
+        uint256 deadline = block.timestamp + 15 minutes;
 
-    // Set the amount of ETH to receive
-    uint256 amountOut = 1 ether;
+        // Create a path for the swap
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = weth;
 
-    // Set the deadline for the transaction
-    uint256 deadline = block.timestamp + 15 minutes;
-
-    // Create a path for the swap
-    address[] memory path = new address[](2);
-    path[0] = token;
-    path[1] = weth;
-
-    // Use the Uniswap V2 Router to swap the token for ETH
-    IUniswapV2Router02(uniswapV2Router).swapExactTokensForTokensSupportingFeeOnTransferTokens(
-      amountIn,
-      amountOut,
-      path,
-      _dex,
-      deadline
-    );
-  }
+        // Use the Uniswap V2 Router to swap the token for ETH
+        IUniswapV2Router02(uniswapV2Router).swapExactTokensForTokensSupportingFeeOnTransferTokens(amountIn, amountOut, path, _dex, deadline);
+    }
 }
-
